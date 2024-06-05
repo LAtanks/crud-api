fdb4bc92b79133fd99ce4ef48d637c9ba6040428
LAtanks <giovannisamuel27@gmail.com>
2024-06-05 19:51:04 -0300

    Create Global exception

--- src/main/java/br/com/latanks/crud_api/infra/GlobalExceptionHandler.java
+++ src/main/java/br/com/latanks/crud_api/infra/GlobalExceptionHandler.java
@@ -1,4 +1,15 @@
 package br.com.latanks.crud_api.infra;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Void> EntityNotFoundException(){
+        return ResponseEntity.badRequest().build();
+    }
 }
