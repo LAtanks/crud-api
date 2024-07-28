0430197af3ba0f0a7f8791ffc6f3ffcff2acefb9
LAtanks <76797064+LAtanks@users.noreply.github.com>
2024-07-28 09:37:51 -0300

    Create User Repository

--- src/main/java/br/com/latanks/crud_api/repositories/UserRepository.java
+++ src/main/java/br/com/latanks/crud_api/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package br.com.latanks.crud_api.repositories;
 import br.com.latanks.crud_api.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserModel, Long> {
+public interface UserRepository {
 }
